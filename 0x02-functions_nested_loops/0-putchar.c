6ebd593ca3ce0698a494dd6416e8c5fe5576fc14
hooleymass <hockleymasese3@gmail.com>
2022-12-13 18:53:35 +0300

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include "Holberton.h"
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "Holberton";
+	int i;
 
-int main() {
-  printf("_putchar\n");
-  return 0;
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
