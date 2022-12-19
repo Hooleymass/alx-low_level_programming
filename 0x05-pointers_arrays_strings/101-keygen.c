d58037da65d99a22b563127df5edcd4f82145901
hooleymass <hockleymasese3@gmail.com>
2022-12-19 23:30:51 +0300

    Learning on my own

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,17 +9,11 @@
  */
 int main(void)
 {
-  // Seed the random number generator with the current time
   srand(time(NULL));
-
-  // Generate a random number between 1 and 10
   int r = rand() % 10 + 1;
-
-  // Generate a random password using the random number
   char password[9];
   sprintf(password, "%d%d%d%d%d%d%d%d", r, r + 1, r + 2, r + 3, r + 4, r + 5, r + 6, r + 7);
 
-  // Print the password
   printf("%s\n", password);
 
   return (0);
