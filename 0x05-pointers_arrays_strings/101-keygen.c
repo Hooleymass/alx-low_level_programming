029904149d69d7524578262efdd81a41c9d3be89
hooleymass <hockleymasese3@gmail.com>
2022-12-20 00:08:22 +0300

    Learning on my own

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,36 +1,57 @@
+/**
+ * 101-keygen.c - Generates a random valid password for the program 101-crackme
+ *
+ * Author: Your Name <your@email.com>
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define PASSWORD_LENGTH 10
 
-int is_valid_password(int password[PASSWORD_LENGTH]) {
+int is_valid_password(void)
+{
     /**
      * Check if the password is valid according to the requirements of the program 101-crackme
      * Return 1 if the password is valid, 0 otherwise
      */
+    return 0;
 }
 
-int main(void) {
+int main(void)
+{
     int password[PASSWORD_LENGTH];
 
-    /* Seed the random number generator with the current time */
+    /**
+     * Seed the random number generator with the current time
+     */
     srand(time(0));
 
-    while (1) {
-        /* Generate a random password */
-        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+    while (1)
+    {
+        /**
+	 * Generate a random password
+	 */
+        for (int i = 0; i < PASSWORD_LENGTH; i++)
+        {
             password[i] = rand() % 10;
         }
 
-        /* Check if the password is valid */
-        if (is_valid_password(password)) {
+        /**
+	 * Check if the password is valid
+	 */
+        if (is_valid_password())
+        {
             break;
         }
     }
 
-    /* Print the generated password */
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+    /**
+     * Print the generated password
+     */
+    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    {
         printf("%d", password[i]);
     }
     printf("\n");
