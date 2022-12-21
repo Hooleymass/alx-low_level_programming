0b55f84d613ea17cf69dc1fcd6691bbc0e87f294
hooleymass <hockleymasese3@gmail.com>
2022-12-21 22:36:27 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,21 @@
  */
 char *leet(char *s)
 {
+    // Initialize the index variables.
     int i = 0;
     int j = 0;
 
+    // Iterate over each character in the string.
     while (s[i] != '\0')
     {
+        // Iterate over each character in the leet alphabet.
         while (j < 5)
         {
-            if (s[i] == "AEOTLaeotl"[j])
+            // Check if the current character in the string matches a character in the leet alphabet.
+            if (s[i] == 'AEOTLaeotl'[j])
             {
-                s[i] = "4307143071"[j];
+                // If a match is found, replace the character in the string with the corresponding character in the leet alphabet.
+                s[i] = '4307143071'[j];
                 break;
             }
             j++;
@@ -29,13 +34,25 @@ char *leet(char *s)
     return s;
 }
 
+/**
+ * main - Test the leet function.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
+    // Declare a string to convert.
     char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    // Declare a pointer to hold the converted string.
     char *p;
+
+    // Convert the string.
     p = leet(s);
+    // Print the converted string.
     printf("%s", p);
+    // Print the original string for comparison.
     printf("%s", s);
+    // Return 0 to indicate success.
     return (0);
 }
 
