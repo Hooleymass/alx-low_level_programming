e0ffab0111d5a4096ed37476de8e4f94678b1ff6
hooleymass <hockleymasese3@gmail.com>
2022-12-21 23:40:43 +0300

    main.h

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-    int n;
-    int a[5];
+    int n = 0;
+    int a[5] = {0};
     int *p;
 
     a[2] = 98;
-    p = &n;
-    printf("a[2] = %d\n", *(p + 2));
+    p = a;
+    printf("a[2] = %d\n", a[2]);
     return (0);
 }
 
