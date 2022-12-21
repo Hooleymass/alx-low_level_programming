7f42457b93bdcd98be1571c7f66abbd8ca887ff3
hooleymass <hockleymasese3@gmail.com>
2022-12-21 23:08:14 +0300

    Advancing

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * 102-magic.c - program that prints a[2] = 98, followed by a new line
+ *
+ * Author: Hooley Mass
+ * Last modified: Jan 21, 2022
+ */
+int main(void)
+{
+    int n;
+    int a[5];
+    int *p;
+
+    a[2] = 1024;
+    p = &n;
+    printf("a[2] = %d\n", *(p + 2));
+    return (0);
+}
+
