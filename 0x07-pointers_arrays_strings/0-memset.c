a900cb799e2ac5e0a3c073b9292d176cf29203f0
hooleymass <hockleymasese3@gmail.com>
2023-01-03 13:10:47 -0800

    NEvER done

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - fills the first n bytes of the memory area pointed to by s
- * with the constant byte b
- * @s: pointer to memory area to fill
- * @b: constant byte to fill memory with
- * @n: number of bytes to fill
- *
+ * _memset - fills memory with constant byte
+ * @s: pointer to the conatant
+ * @b: constant
+ * @n: maximum byte to use
  * Return: pointer to memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
-
-    return s;
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
 
