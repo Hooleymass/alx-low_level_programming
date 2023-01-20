e26b507f8590c81a27c3106620b3124eb3b34ff9
hooleymass <hockleymasese3@gmail.com>
2023-01-20 09:20:22 +0300

    initial commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,27 @@
 #include "variadic_functions.h"
-/**
- * sum_them_all - return sum of all the parameters.
- * @n: name of the last parament
- *
- * Return: the sum
- */
+#include <stdarg.h>
 
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: name of the last parameter
+ *
+ * Return: sum
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list hooley;
+	va_list ap;
 	unsigned int i, sum;
 
-	va_start(hooley, sum);
+	va_start(ap, n);
 	sum = 0;
 
 	if (n == 0)
 	{
-		return 0;
+		return (0);
 	}
 	for (i = 0; i < n; i++)
-		sum += va_arg(hooley, unsigned int);
-	va_end(hooley);
+		sum += va_arg(ap, unsigned int);
+	va_end(ap);
 
-	return sum;
-
+	return (sum);
 }
